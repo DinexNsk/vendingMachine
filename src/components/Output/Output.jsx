c2f8b045dbc4d413731324cfe6b6093123074d1f
Dmitry Makarov <andrew.ovchenkov@gmail.com>
2020-07-07 13:05:21 +0700

    make small optimization

--- src/components/Output/Output.jsx
+++ src/components/Output/Output.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import styles from './Output.module.scss';
 import { Item } from '../Item/Item';
@@ -26,10 +26,18 @@ export const Output = ({
     const textStatus = boughtItem ? 'Take your product and change!' : '';
     const changeResult = minCoinChange(coins, moneyChange);
 
-    const displayChange = boughtItem ? (moneyChange ? Object.keys(changeResult).sort((a, b) => +b - +a).map(el => 
-    <div key={`changeId-${el}`} style={{ marginBottom: 10}}>
-        {`${el} R: ${changeResult[el]} coins`}
-    </div>) : 'No change!') : '';
+    const showChangeText = useCallback(() => {
+        if (!moneyChange) {
+            return 'No change!'
+        }
+        return Object.keys(changeResult).sort((a, b) => Number(b) - Number(a)).map(el =>
+            <div key={`changeId-${el}`} style={{ marginBottom: 10 }}>
+                {`${el} R: ${changeResult[el]} coins`}
+            </div>);
+    }, [changeResult, moneyChange])
+
+    const displayChange = boughtItem ? showChangeText() : '';
+
     return (
         <div className={styles.output}>
             <div className={styles.output__message}>
